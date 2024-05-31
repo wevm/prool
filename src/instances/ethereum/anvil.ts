644ae7e0f85acefa6bfee6b3611e24eff9ca4cd5
jxom <jakemoxey@gmail.com>
2024-05-31 13:25:26 +1000

    chore: more params

--- src/instances/ethereum/anvil.ts
+++ src/instances/ethereum/anvil.ts
@@ -3,35 +3,51 @@ import { stripColors } from '../../utils/stripColors.js'
 import { toArgs } from '../../utils/toArgs.js'
 import { defineInstance } from '../defineInstance.js'
 
-type Hardfork =
-  | 'Frontier'
-  | 'Homestead'
-  | 'Dao'
-  | 'Tangerine'
-  | 'SpuriousDragon'
-  | 'Byzantium'
-  | 'Constantinople'
-  | 'Petersburg'
-  | 'Istanbul'
-  | 'Muirglacier'
-  | 'Berlin'
-  | 'London'
-  | 'ArrowGlacier'
-  | 'GrayGlacier'
-  | 'Paris'
-  | 'Shanghai'
-  | 'Cancun'
-  | 'Latest'
-
 export type AnvilParameters = {
+  /**
+   * Number of dev accounts to generate and configure.
+   *
+   * @defaultValue 10
+   */
+  accounts?: number | undefined
+  /**
+   * Set the Access-Control-Allow-Origin response header (CORS).
+   *
+   * @defaultValue *
+   */
+  allowOrigin?: string | undefined
   /**
    * Enable autoImpersonate on startup
    */
   autoImpersonate?: boolean | undefined
   /**
+   * The balance of every dev account in Ether.
+   *
+   * @defaultValue 10000
+   */
+  balance?: number | bigint | undefined
+  /**
+   * The base fee in a block.
+   */
+  blockBaseFeePerGas?: number | bigint | undefined
+  /**
+   * Block time in seconds for interval mining.
+   */
+  blockTime?: number | undefined
+  /**
    * Path or alias to the Anvil binary.
    */
   binary?: string | undefined
+  /**
+   * The chain id.
+   */
+  chainId?: number | undefined
+  /**
+   * EIP-170: Contract code size limit in bytes. Useful to increase this because of tests.
+   *
+   * @defaultValue 0x6000 (~25kb)
+   */
+  codeSizeLimit?: number | undefined
   /**
    * Sets the number of assumed available compute units per second for this fork provider.
    *
@@ -39,6 +55,25 @@ export type AnvilParameters = {
    * @see https://github.com/alchemyplatform/alchemy-docs/blob/master/documentation/compute-units.md#rate-limits-cups
    */
   computeUnitsPerSecond?: number | undefined
+  /**
+   * Writes output of `anvil` as json to user-specified file.
+   */
+  configOut?: string | undefined
+  /**
+   * Sets the derivation path of the child key to be derived.
+   *
+   * @defaultValue m/44'/60'/0'/0/
+   */
+  derivationPath?: string | undefined
+  /**
+   * Disable the `call.gas_limit <= block.gas_limit` constraint.
+   */
+  disableBlockGasLimit?: boolean | undefined
+  /**
+   * Dump the state of chain on exit to the given file. If the value is a directory, the state will be
+   * written to `<VALUE>/state.json`.
+   */
+  dumpState?: string | undefined
   /**
    * Fetch state over a remote endpoint instead of starting from an empty state.
    *
@@ -72,49 +107,6 @@ export type AnvilParameters = {
    */
   forkRetryBackoff?: number | undefined
   /**
-   * Disables rate limiting for this node's provider.
-   *
-   * @defaultValue false
-   * @see https://github.com/alchemyplatform/alchemy-docs/blob/master/documentation/compute-units.md#rate-limits-cups
-   */
-  noRateLimit?: boolean | undefined
-  /**
-   * Explicitly disables the use of RPC caching.
-   *
-   * All storage slots are read entirely from the endpoint.
-   */
-  noStorageCaching?: boolean | undefined
-  /**
-   * Number of retry requests for spurious networks (timed out requests).
-   *
-   * @defaultValue 5
-   */
-  retries?: number | undefined
-  /**
-   * Timeout in ms for requests sent to remote JSON-RPC server in forking mode.
-   *
-   * @defaultValue 45000
-   */
-  timeout?: number | undefined
-  /**
-   * The base fee in a block.
-   */
-  blockBaseFeePerGas?: number | bigint | undefined
-  /**
-   * The chain id.
-   */
-  chainId?: number | undefined
-  /**
-   * EIP-170: Contract code size limit in bytes. Useful to increase this because of tests.
-   *
-   * @defaultValue 0x6000 (~25kb)
-   */
-  codeSizeLimit?: number | undefined
-  /**
-   * Disable the `call.gas_limit <= block.gas_limit` constraint.
-   */
-  disableBlockGasLimit?: boolean | undefined
-  /**
    * The block gas limit.
    */
   gasLimit?: number | bigint | undefined
@@ -123,64 +115,28 @@ export type AnvilParameters = {
    */
   gasPrice?: number | bigint | undefined
   /**
-   * Number of dev accounts to generate and configure.
-   *
-   * @defaultValue 10
-   */
-  accounts?: number | undefined
-  /**
-   * The balance of every dev account in Ether.
-   *
-   * @defaultValue 10000
-   */
-  balance?: number | bigint | undefined
-  /**
-   * Sets the derivation path of the child key to be derived.
-   *
-   * @defaultValue m/44'/60'/0'/0/
-   */
-  derivationPath?: string | undefined
-  /**
-   * BIP39 mnemonic phrase used for generating accounts.
-   */
-  mnemonic?: string | undefined
-  /**
-   * Port number to listen on.
-   *
-   * @defaultValue 8545
-   */
-  port?: number | undefined
-  /**
-   * Enable steps tracing used for debug calls returning geth-style traces.
-   */
-  stepsTracing?: boolean | undefined
-  /**
-   * The timestamp of the genesis block.
-   */
-  timestamp?: number | bigint | undefined
-  /**
-   * Set the Access-Control-Allow-Origin response header (CORS).
-   *
-   * @defaultValue *
-   */
-  allowOrigin?: string | undefined
-  /**
-   * Block time in seconds for interval mining.
-   */
-  blockTime?: number | undefined
-  /**
-   * Writes output of `anvil` as json to user-specified file.
-   */
-  configOut?: string | undefined
-  /**
-   * Dump the state of chain on exit to the given file. If the value is a directory, the state will be
-   * written to `<VALUE>/state.json`.
-   */
-  dumpState?: string | undefined
-  /**
    * The EVM hardfork to use.
    */
-  hardfork?: Hardfork | undefined
+  hardfork?:
+    | 'Frontier'
+    | 'Homestead'
+    | 'Dao'
+    | 'Tangerine'
+    | 'SpuriousDragon'
+    | 'Byzantium'
+    | 'Constantinople'
+    | 'Petersburg'
+    | 'Istanbul'
+    | 'Muirglacier'
+    | 'Berlin'
+    | 'London'
+    | 'ArrowGlacier'
+    | 'GrayGlacier'
+    | 'Paris'
+    | 'Shanghai'
+    | 'Cancun'
+    | 'Latest'
+    | undefined
   /**
    * The host the server will listen on.
    */
@@ -198,6 +154,14 @@ export type AnvilParameters = {
    */
   loadState?: string | undefined
   /**
+   * BIP39 mnemonic phrase used for generating accounts.
+   */
+  mnemonic?: string | undefined
+  /**
+   * Automatically generates a BIP39 mnemonic phrase, and derives accounts from it.
+   */
+  mnemonicRandom?: boolean | undefined
+  /**
    * Disable CORS.
    */
   noCors?: boolean | undefined
@@ -206,28 +170,75 @@ export type AnvilParameters = {
    */
   noMining?: boolean | undefined
   /**
+   * Disables rate limiting for this node's provider.
+   *
+   * @defaultValue false
+   * @see https://github.com/alchemyplatform/alchemy-docs/blob/master/documentation/compute-units.md#rate-limits-cups
+   */
+  noRateLimit?: boolean | undefined
+  /**
+   * Explicitly disables the use of RPC caching.
+   *
+   * All storage slots are read entirely from the endpoint.
+   */
+  noStorageCaching?: boolean | undefined
+  /**
    * How transactions are sorted in the mempool.
    *
    * @defaultValue fees
    */
   order?: string | undefined
   /**
+   * Run an Optimism chain.
+   */
+  optimism?: boolean | undefined
+  /**
+   * Port number to listen on.
+   *
+   * @defaultValue 8545
+   */
+  port?: number | undefined
+  /**
    * Don't keep full chain history. If a number argument is specified, at most this number of states is kept in memory.
    */
   pruneHistory?: number | undefined | boolean
   /**
-   * Interval in seconds at which the status is to be dumped to disk.
+   * Number of retry requests for spurious networks (timed out requests).
+   *
+   * @defaultValue 5
    */
-  stateInterval?: number | undefined
+  retries?: number | undefined
   /**
    * Don't print anything on startup and don't print logs.
    */
   silent?: boolean | undefined
   /**
+   * Slots in an epoch.
+   */
+  slotsInAnEpoch?: number | undefined
+  /**
+   * Enable steps tracing used for debug calls returning geth-style traces.
+   */
+  stepsTracing?: boolean | undefined
+  /**
+   * Interval in seconds at which the status is to be dumped to disk.
+   */
+  stateInterval?: number | undefined
+  /**
    * This is an alias for both `loadState` and `dumpState`. It initializes the chain with the state stored at the
    * file, if it exists, and dumps the chain's state on exit
    */
   state?: string | undefined
+  /**
+   * Timeout in ms for requests sent to remote JSON-RPC server in forking mode.
+   *
+   * @defaultValue 45000
+   */
+  timeout?: number | undefined
+  /**
+   * The timestamp of the genesis block.
+   */
+  timestamp?: number | bigint | undefined
   /**
    * Number of blocks with transactions to keep in memory.
    */
