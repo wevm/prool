75382d155ab18d231d58d74510c3ce4a4da56aea
jxom <jakemoxey@gmail.com>
2024-06-05 15:00:07 +1000

    chore: export `stackup`

---
+++ .changeset/strange-bats-poke.md
@@ -0,0 +1,5 @@
+---
+"prool": patch
+---
+
+Exported `stackup`.
--- src/exports/instances.ts
+++ src/exports/instances.ts
@@ -1,1 +1,2 @@
 export { type AnvilParameters, anvil } from '../instances/anvil.js'
+export { type StackupParameters, stackup } from '../instances/stackup.js'
